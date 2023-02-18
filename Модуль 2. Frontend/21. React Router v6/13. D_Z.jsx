f9b6991c03fa2e61397be34cc68aa0de1da3bac8
Aleksander <asava@sibmail.com>
2023-02-18 19:31:43 +0700

    ok

---
+++ Модуль 2. Frontend/21. React Router v6/13. D_Z.jsx
@@ -0,0 +1,40 @@
+// Проект на React Router 5: ссылка
+// https://drive.google.com/drive/folders/1PzIfz4wv0vUIWuw0VwzSDRmtIwRtDpaq?usp=
+
+// Проект на React Router 6: ссылка
+//  https://drive.google.com/drive/folders/1XBH6m1eWqmpuppYqVtKYVnpPpI2BtWzx?usp=sharing
+
+
+
+// Урок React Router 6, 
+
+// 1. Create app with RRD v5 and 
+
+// 2. Implement routes as described below:
+// /  - Home page
+// /users
+// 	/ - users list
+// 	/:userId
+// 		/ - redirect to profile
+// 		/profile - user info
+// 		/edit
+// 		* redirect to info
+// * redirect to home
+
+
+
+
+// 3. Implement navigation as described below:
+
+// Home Page
+// 	1. NavLink to user list
+// Users List page
+// 	1. NavLink to user page
+// 	2. NavLink to home page
+// User page
+// 	1. NavLink to user edit page
+// 	2. NavLink to users list
+// User edit page
+// 	1. NavLink to user page
+// 	2. NavLink to another user page
+// 	3. NavLink to Users list page
