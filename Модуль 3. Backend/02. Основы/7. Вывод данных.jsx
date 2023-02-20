063ce3c299c13ef61f8835490d7b4dffde03c413
Aleksander <asava@sibmail.com>
2023-02-20 14:09:37 +0700

    ok

---
+++ Модуль 3. Backend/02. Основы/7. Вывод данных.jsx
@@ -0,0 +1,7 @@
+// // Исходные файлы: ссылка
+// https://drive.google.com/drive/folders/1LZQ6I0lKuZiLzNYGnfHH79P9ZHpI9G6H?usp=sharing
+
+// Обрати внимание!
+
+// Для корректной работы "chalk" необходимо установить версию 4 при помощи команды:
+// npm i chalk@4.1.2
