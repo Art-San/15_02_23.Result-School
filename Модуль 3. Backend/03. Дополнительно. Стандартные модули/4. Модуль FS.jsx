7e2fdb7e049de1fbea9cc5003ec760d3dedad04d
Aleksander <asava@sibmail.com>
2023-02-21 14:16:30 +0700

    ok

---
+++ Модуль 3. Backend/03. Дополнительно. Стандартные модули/4. Модуль FS.jsx
@@ -0,0 +1,5 @@
+//fs- взаимодействия с файловой системой способом, 
+// смоделированным на основе стандартных функций POSIX
+
+// Исходные файлы: ссылка
+// https://drive.google.com/drive/folders/1o6z03Fj9nAsWJEmQBiHFkKVu37qb5E3l?usp=sharing
