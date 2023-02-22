291148277dcc4a33fac6aa15c71ea8d83b879c5a
Aleksander <asava@sibmail.com>
2023-02-22 13:30:02 +0700

    ok

---
+++ Модуль 3. Backend/04. Web-сервер/2. Автоматизация с Nodemon.jsx
@@ -0,0 +1,28 @@
+// Nodemon
+// npm i nodemon -D (-D для сохраннения как зависимости для раработки)
+
+
+// {
+//     "name": "note-app",
+//     "version": "1.0.0",
+//     "description": "This is simple NodeJs Application",
+//     "main": "index.js",
+//     "scripts": {
+//       "start": "node index.js",    // вобавили  // npm start
+//       "server": "nodemon inrex.js"  // npm run server
+//     },
+//     "keywords": [
+//       "node",
+//       "javascript"
+//     ],
+//     "author": "Art-San",
+//     "license": "ISC",
+//     "dependencies": {
+//       "chalk": "^4.1.2",
+//       "yargs": "^17.7.0"
+//     },
+//     "devDependencies": {
+//       "@types/yargs": "^17.0.22",
+//       "nodemon": "^2.0.20"
+//     }
+//   }
