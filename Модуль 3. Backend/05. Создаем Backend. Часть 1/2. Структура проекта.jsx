6be7248a89a2d5303bf0aee057207637f908dbf3
Aleksander <asava@sibmail.com>
2023-02-23 18:47:04 +0700

    ok

---
+++ Модуль 3. Backend/05. Создаем Backend. Часть 1/2. Структура проекта.jsx
@@ -0,0 +1,2 @@
+// Файлы к уроку: ссылка
+// https://drive.google.com/drive/folders/1fLUWFeILew1xCiwA5YzvzAchCcjxBFYQ
---
+++ Модуль 3. Backend/05. Создаем Backend. Часть 1/3. Настройка сервера.jsx
@@ -0,0 +1,6 @@
+// 1. npm init в папке сервер создаем файл package.json
+
+// 2. npm install express config chalk mongoose
+
+// config
+// https://www.npmjs.com/package/config
