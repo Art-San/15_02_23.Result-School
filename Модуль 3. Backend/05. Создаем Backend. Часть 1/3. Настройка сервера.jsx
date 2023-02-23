10b4900cdc789ada9fb4923f333e7d3b4d2401d7
Aleksander <asava@sibmail.com>
2023-02-23 21:07:13 +0700

    ok

--- Модуль 3. Backend/05. Создаем Backend. Часть 1/3. Настройка сервера.jsx
+++ Модуль 3. Backend/05. Создаем Backend. Часть 1/3. Настройка сервера.jsx
@@ -1,6 +1,11 @@
+// 0. npm uninstall chalk
+
 // 1. npm init в папке сервер создаем файл package.json
 
 // 2. npm install express config chalk mongoose
+
+// 3. npm install -D nodemon   // все наши изменения будут видется и сервер будет перезагружатся
+// 4. npm install -D cross-env // помогает задавать инваринет инваринет переменые
 
 // config
 // https://www.npmjs.com/package/config
