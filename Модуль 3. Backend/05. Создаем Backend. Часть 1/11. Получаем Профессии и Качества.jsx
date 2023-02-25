ad774861222394f0704341d02ac47160e9c1d8b4
Aleksander <asava@sibmail.com>
2023-02-25 13:55:00 +0700

    ok

--- Модуль 3. Backend/05. Создаем Backend. Часть 1/11. Получаем Профессии и Качества.jsx
+++ Модуль 3. Backend/05. Создаем Backend. Часть 1/11. Получаем Профессии и Качества.jsx
@@ -1,2 +1,5 @@
 // JSONVue: ссылка
 // https://chrome.google.com/webstore/detail/jsonvue/chklaanhfefbnpoihckbnefhakgolnmc
+
+// ApiEndpoints: ссылка
+// https://vladilen.notion.site/Api-Endpoints-9c0dc0d186c84447b293c0ac986e6c00
