31596ee3a49a8c6dca603dce43cee7a2c8ab0e07
Aleksander <asava@sibmail.com>
2023-02-25 17:55:51 +0700

    ok

---
+++ Модуль 3. Backend/05. Создаем Backend. Часть 1/12. Что такое Postman.jsx
@@ -0,0 +1,6 @@
+// Postman: ссылка
+// https://www.postman.com/downloads/
+
+
+// Что такое Postman?
+// HTTP-клиент для тестирования API
---
+++ Модуль 3. Backend/06. Создаем Backend. Часть 2/1. Пишем логику для регистрации.jsx
@@ -0,0 +1,13 @@
+// Файлы к практике: ссылка
+// https://vladilen.notion.site/d59208ea56ad4b50b5c9081deabcbcbb
+
+// ApiEndpoints: ссылка
+// https://vladilen.notion.site/Api-Endpoints-9c0dc0d186c84447b293c0ac986e6c00
+
+// Доп пакеты для реализаци регистрации
+
+npm install bcryptjs jsonwebtoken express-validator
+
+npm i bcryptjs
+npm i jsonwebtoken
+npm i express-validator
