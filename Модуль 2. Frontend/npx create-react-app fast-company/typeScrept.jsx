bd98733f07b33a48b6f6c0021947fc6e9a7e4f72
Aleksander <asava@sibmail.com>
2023-02-26 18:53:18 +0700

    ok

--- Модуль 2. Frontend/npx create-react-app fast-company/typeScrept.jsx
+++ Модуль 2. Frontend/npx create-react-app fast-company/typeScrept.jsx
@@ -2,3 +2,9 @@ npx create-react-app --template typescript .
 // Точка в конце обозначает что проэкт установится 
 // в ту папку в которой сейчас ноходимся
 (точка чето не сработала пришлось добавлять название вместо точки)
+
+
+// https://www.youtube.com/watch?v=PpUxVieVzeg&t=190s
+// npx create-react-app my-todo-app --template typescript
+
+npx create-react-app . --template typescript // точка по середине
