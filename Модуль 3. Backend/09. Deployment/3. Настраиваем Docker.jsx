bb2399848e4e095e00612911373a2be1cad987b9
Aleksander <asava@sibmail.com>
2023-03-03 12:43:32 +0700

    ok

---
+++ Модуль 3. Backend/09. Deployment/3. Настраиваем Docker.jsx
@@ -0,0 +1,59 @@
+docker -v
+
+// .dockerignore - nod_mudules и другие
+// Dockerfile - инструкция для докера
+
+Обрати внимание!
+
+Во избежание ошибок Node.js на 07:15 мы также рекомендуем использовать 
+LTS версию Node.js (FROM node:16-alpine)
+
+Дополнительные материалы:
+
+// Docker Desktop: ссылка
+// https://www.docker.com/products/docker-desktop
+// Docker: ссылка
+// https://www.youtube.com/watch?v=n9uCgUzfeRQ&feature=youtu.be
+// Как ученик школы ты можете приобрести курс по Docker со скидкой 30%. 
+// Для этого напишите в службу поддержки курса сообщение с промокодом dockerstudent 
+// и тебе помогут оплатить данный курс со скидкой
+
+// устанавливаем докер на комп
+// https://www.docker.com/products/docker-desktop/
+
+// Как включить виртуализацию в Windows 10
+// https://www.youtube.com/watch?v=M97qZHykCXQ
+статья
+// Если у вас процессор Intel, нажмите здесь.
+// https://support.bluestacks.com/hc/ru/articles/115003174386-%D0%9A%D0%B0%D0%BA-%D0%B2%D0%BA%D0%BB%D1%8E%D1%87%D0%B8%D1%82%D1%8C-%D0%B0%D0%BF%D0%BF%D0%B0%D1%80%D0%B0%D1%82%D0%BD%D1%83%D1%8E-%D0%B2%D0%B8%D1%80%D1%82%D1%83%D0%B0%D0%BB%D0%B8%D0%B7%D0%B0%D1%86%D0%B8%D1%8E-VT-%D0%BD%D0%B0-%D0%9F%D0%9A-#%E2%80%9C9%E2%80%9D
+
+
+https://www.youtube.com/watch?v=PHYRSPCD69U
+
+// от Администратора
+// systeminfo
+
+// в командной строке смотрим проц (свеиние о системе)
+// msinfo32
+
+Процессор	Intel(R) Pentium(R) CPU G2120 @ 3.10GHz, 3100 МГц, ядер: 2, логических процессоров: 2
+
+https://inthetechpit.com/2021/01/27/docker-desktop-requires-the-server-service-to-be-enabled-windows-10/
+
+//=========================================================================================
+// Обнаружена низкоуровневая оболочка. Функции, необходимые для Hyper-V, отображены не будут
+// решение // https://qna.habr.com/q/555555
+msinfo32
+//=================================================================
+
+// от Администратора служба сервера
+Services.msc
+
+
+Docker Desktop requires the Workstation service to be enabled.
+
+Установка Linux на Windows с помощью WSL
+https://learn.microsoft.com/ru-ru/windows/wsl/install
+
+1. Use WSL 2 instead of Hyper-v 
+2. Add shortcut to desktop
