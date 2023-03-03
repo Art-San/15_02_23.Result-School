ff3ca9cc4d777742f26f3e97e63700bde9eedc2e
Aleksander <asava@sibmail.com>
2023-03-03 18:36:01 +0700

    ok

---
+++ Модуль 3. Backend/09. Deployment/4. Запускаем приложение в Docker контейнере.jsx
@@ -0,0 +1,31 @@
+docker -v
+// docker build -t jfd . (-t-называем, (.)-в конце указывает где искать файл инструкцию Dockerfile (в этой папке))
+clear
+
+docker image ls
+REPOSITORY   TAG       IMAGE ID       CREATED       SIZE
+jfd          latest    033bab48ced6   3 hours ago   196MB
+
+строим контейнер 
+
+docker run -d -p 3000:8080 --name jfd --rm jfd  // тест какойто
+
+docker ps -a
+
+docker stop jfd
+
+clear
+
+docker run -d -p 8080:8080 --name jfd --rm jfd // Запускаем с привычным портом 8080:8080
+
+docker stop jfd // ФИНАЛ УРОКА
+
+// ===========================================================
+Сохраните изменения в вашем Dockerfile, 
+а затем попробуйте снова создать образ с помощью команды        // ПОМОЩЬ ИИ
+docker build -t my-image-name .. Это должно работать сейчас.
+// ==================================================================
+
+
+// => CANCELED [stage-1 4/6] RUN npm instal                                                                               7.9s 
+// => ERROR [client 4/6] RUN npm instal    
