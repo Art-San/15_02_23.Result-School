aaf8e4ea8945ae466cf1c4c1c65678fc00898fa0
Aleksander <asava@sibmail.com>
2023-03-17 23:24:56 +0700

    ok

---
+++ Модуль 3. Backend/01. Введение/1. Что мы будем делать в рамках этого модуля.jsx
@@ -0,0 +1,4 @@
+// При желании ты можешь выполнять deploy 
+// своих проектов при помощи облачной платформы Heroku: инструкция
+
+// https://vladilen.notion.site/Deployment-Full-Stack-8d0211d965ad4ce688d43c9f474b1918
