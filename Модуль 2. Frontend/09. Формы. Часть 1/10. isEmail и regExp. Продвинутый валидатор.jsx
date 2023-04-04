eb03437a1f921baa5f7be4a92dab1d733183ffe1
Aleksander <asava@sibmail.com>
2023-04-04 18:58:45 +0700

    ok

---
+++ Модуль 2. Frontend/09. Формы. Часть 1/10. isEmail и regExp. Продвинутый валидатор.jsx
@@ -0,0 +1,4 @@
+// regExp: ссылка
+// https://regex101.com/
+// regExp - краткое руководство: ссылка
+// https://docs.microsoft.com/en-us/dotnet/standard/base-types/regular-expression-language-quick-reference
