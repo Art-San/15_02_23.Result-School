c276438a6315f8fd683d6f419e92f8cace53127b
Aleksander <asava@sibmail.com>
2023-04-06 12:52:59 +0700

    ok

---
+++ Модуль 2. Frontend/10. Формы. Часть 2/0. Введение.jsx
@@ -0,0 +1,21 @@
+Цель урока:
+
+1. Научиться создавать переиспользуемые компоненты: select, multiselect, radio, checkbox
+2. Понять, как правильно структурировать компоненты
+
+Содержание урока:
+
+     1. Рефакторинг компонентов
+     2. Создание компонентов авторизации и регистрации
+     3. Компоненты select, multiselect, radio, checkbox
+     4. Валидация с использованием Yup
+
+
+Дополнительные материалы:
+
+// React-select: ссылка
+// https://react-select.com/home
+// Yup: ссылка
+// https://www.npmjs.com/package/yup
+// Что такое LocalStorage: ссылка
+// https://www.youtube.com/watch?v=3-bZ7gLVSzo
