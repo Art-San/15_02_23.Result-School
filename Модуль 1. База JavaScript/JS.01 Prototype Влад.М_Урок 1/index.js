8d09dc218880968af62c28d5bb10d432dbe9b123
Aleksander <asava@sibmail.com>
2023-04-17 16:22:45 +0700

    ok

--- Модуль 1. База JavaScript/JS.01 Prototype Влад.М_Урок 1/index.js
+++ Модуль 1. База JavaScript/JS.01 Prototype Влад.М_Урок 1/index.js
@@ -1,9 +1,8 @@
 // Ссылка на урок
 // https://www.youtube.com/watch?v=aQkgUUmUJy4&t=83s
 
-
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-// prototype- Это некоторый объект с помощью которого мы рассширяем 
+// prototype- Это некоторый объект с помощью которого мы рассширяем
 // свойства объектоа или классов, и с помощью его можем устраивать определеное наследование
 // в нутри JS
 //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -16,23 +15,22 @@
 //     }
 // }
 
+// const person = new Object({
+//         name: 'Maxim',
+//         age: 25,
+//         greet: function () {
+//             console.log('Greet!')
+//         }
+//     }
+// )
 
+// Object.prototype.sayHello = function() {
+//     console.log('Hello')
+// }
 
-const person = new Object({
-        name: 'Maxim',
-        age: 25,
-        greet: function () {
-            console.log('Greet!')
-        }
-    }
-)
+// const lena = Object.create(person)
+// lena.name = 'Elena'
 
-Object.prototype.sayHello = function() {
-    console.log('Hello')
-}
+// const str = new String('I am string')
 
-const lena = Object.create(person)
-lena.name = 'Elena'
-
-const str = new String('I am string')
-
+// gruzz70.maska
