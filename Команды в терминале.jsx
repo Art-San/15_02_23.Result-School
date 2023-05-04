9cde5cf98eaefdc62aa022fdc7a9c2e60cb262e1
Aleksander <asava@sibmail.com>
2023-05-04 12:02:36 +0700

    ok

--- Команды в терминале.jsx
+++ Команды в терминале.jsx
@@ -8,3 +8,14 @@
 // rmdir YTb_Telegram_bot_that_talks_to_ChatGPT_API.Vladilen_M // удалит пустой каталог
 // rm -r YTb_Telegram_bot_that_talks_to_ChatGPT_API.Vladilen_M // удалит каталог с файлами
 //============================================================================================
+
+// 2. ПЕРЕМЕЩЕНИЕ
+// ----- cd -------
+// cd YTb_Telegram_bot_that_talks_to_ChatGPT_API.Vladilen_M // поподаем в нужную папку
+// ---- cd .. --- 
+// cd .. // на один уровень вверх
+
+// 3. СОЗДАТЬ фаил 
+// -----  touch ------
+// touch production.json
+
