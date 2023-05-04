a0589e19333fe5683a905f81c6e70ecd439b960c
Aleksander <asava@sibmail.com>
2023-05-04 10:21:30 +0700

    Команды в консоле

---
+++ Команды в терминале.jsx
@@ -0,0 +1,10 @@
+
+// 1. УДАЛЕНИЕ
+//  ----  rm
+// rm YTb_Telegram_bot_that_talks_to_ChatGPT_API.Vladilen_M // удалит фаил
+// rm /path/to/YTb_Telegram_bot_that_talks_to_ChatGPT_API.Vladilen_M // удалит файл по пути
+
+// --------- rmdir -- или ------ rm -r
+// rmdir YTb_Telegram_bot_that_talks_to_ChatGPT_API.Vladilen_M // удалит пустой каталог
+// rm -r YTb_Telegram_bot_that_talks_to_ChatGPT_API.Vladilen_M // удалит каталог с файлами
+//============================================================================================
