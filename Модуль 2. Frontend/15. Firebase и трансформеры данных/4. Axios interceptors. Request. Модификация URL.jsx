37022a004452e3d262c7454227f5b10b10ded5ad
Aleksander <asava@sibmail.com>
2023-06-18 19:22:36 +0700

    ok

--- Модуль 2. Frontend/15. Firebase и трансформеры данных/4. Axios interceptors. Request. Модификация URL.jsx
+++ Модуль 2. Frontend/15. Firebase и трансформеры данных/4. Axios interceptors. Request. Модификация URL.jsx
@@ -1,2 +1,37 @@
 // Документация Axios (Interceptors): ссылка
 // https://axios-http.com/docs/interceptors
+
+// при созданнии базы такие правила были созданы
+{
+    "rules": {
+      ".read": "now < 1689613200000",  // 2023-7-18
+      ".write": "now < 1689613200000",  // 2023-7-18
+    }
+  }
+
+// .. а это правила были уже в конце
+// {
+//     "rules": {
+//       "user": {
+//         ".read": "auth  !=null",
+//         "$uid": {
+//             ".write": "$uid === auth.uid"
+//         }
+//       },
+//       "quality": {
+//         ".read": true,
+//         ".write": false
+//       },
+//       "profession": {
+//         ".read": true,
+//         ".write": false
+//       },
+//       "comment": {
+//         ".read": "auth  !=null",
+//             ".indexOn": ["pageId"],
+//               "$cid": {
+//                  ".write":"auth !=null&& ((data.child('userId').val() === auth.uid) || ( newData.child('userId').val() === auth.uid))"
+//                 }
+//       }
+//     }
+//   }
