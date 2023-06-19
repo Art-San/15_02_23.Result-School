bce0c8469803410f5b31e19fb5ddf9936c16f082
Aleksander <asava@sibmail.com>
2023-06-19 15:46:53 +0700

    ok

--- Модуль 2. Frontend/15. Firebase и трансформеры данных/4. Axios interceptors. Request. Модификация URL.jsx
+++ Модуль 2. Frontend/15. Firebase и трансформеры данных/4. Axios interceptors. Request. Модификация URL.jsx
@@ -10,28 +10,33 @@
   }
 
 // .. а это правила были уже в конце
-// {
-//     "rules": {
-//       "user": {
-//         ".read": "auth  !=null",
-//         "$uid": {
-//             ".write": "$uid === auth.uid"
-//         }
-//       },
-//       "quality": {
-//         ".read": true,
-//         ".write": false
-//       },
-//       "profession": {
-//         ".read": true,
-//         ".write": false
-//       },
-//       "comment": {
-//         ".read": "auth  !=null",
-//             ".indexOn": ["pageId"],
-//               "$cid": {
-//                  ".write":"auth !=null&& ((data.child('userId').val() === auth.uid) || ( newData.child('userId').val() === auth.uid))"
-//                 }
-//       }
-//     }
-//   }
+{
+    "rules": {
+      "user": {
+        ".read": "auth  !=null",
+        "$uid": {
+            ".write": "$uid === auth.uid"
+        }
+      },
+      "quality": {
+        ".read": true,
+        ".write": false
+      },
+      "profession": {
+        ".read": true,
+        ".write": false
+      },
+      "comment": {
+        ".read": "auth  !=null",
+            ".indexOn": ["pageId"],
+              "$cid": {
+                 ".write":"auth !=null&& ((data.child('userId').val() === auth.uid) || ( newData.child('userId').val() === auth.uid))"
+                }
+      }
+    }
+  }
+
+  аутентификация / authentication
+  // говорится 16. Авторизация и аутентификация. Часть 1 
+  // 5. Принципы регистрации в Firebase
+  api-key появляется когда в базе будет создан раздел аутентификация
