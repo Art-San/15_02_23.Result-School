3f4c9e4543776518361fb9858d860d962883e17b
Art San <artsan.mcc@gmail.com>
2024-05-15 12:55:33 +0700

    ok

---
+++ TypeScript/15 - Литеральные типы.ts
@@ -0,0 +1,30 @@
+// Литеральные немного напоминают енам
+
+let c: 'string' = 'string'
+
+type actionType = 'up' | 'down'
+
+const performAction1 = (action: actionType): -1 | 1 => {
+  switch (action) {
+    case 'down':
+      return -1
+    case 'up':
+      //   return 3
+      return 1
+  }
+}
+const performAction2 = (action: actionType | IComplexAction) => {
+  switch (action) {
+    case 'down':
+      return -1
+    case 'up':
+      //   return 3
+      return 1
+  }
+}
+
+// performAction('ter')
+
+interface IComplexAction {
+  s: string
+}
