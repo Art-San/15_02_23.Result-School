297e10e4df551a214d0dab1e0fada0247419349e
Art San <artsan.mcc@gmail.com>
2024-05-15 21:24:57 +0700

    ok

--- TypeScript/16 - Классы.ts
+++ TypeScript/16 - Классы.ts
@@ -8,20 +8,20 @@
 // ===================================
 
 // class Point1 {
-//   x: number // ! означает что мы обязуемся как то за инициализировать это свойство
+//   x: number
 //   y: number
 // }
 
 // const point = new Point1()
-// point.x = 1
+// point.x = 1 // доступно и 'x' и 'y'
 
-// =======================================
-
+// ==================  constructor  =====================
+// Конструктор позволяет задавать изначальные параметры класса
 // class Point1 {
 //   x: number
-//   y: number
+//   y: number | undefined
 
-//   constructor(x: number, y: number) {
+//   constructor(x: number, y?: number) {
 //     this.x = x
 //     this.y = y
 //   }
@@ -32,8 +32,9 @@
 
 // =============== Свойства класса
 // class Point1 {
-//   // private x: number // private будет доступно только внутри объекта класса
-//   readonly x: number // можем только читать но не записывать
+//   //   private x: number // private будет доступно только внутри объекта класса
+//   readonly x: number // можем только читать но не записывать, исключение "constructor"
+//   //   x: number
 //   y: number
 
 //   constructor(x: number, y: number) {
@@ -44,16 +45,15 @@
 //   fun0() {
 //     this.x = 10 // тут можем только Читать, но не записывать
 //   }
-//  private fun1() {
+//   private fun1() {
 //     this.x = 10 // тут можем только Читать, но не записывать
 //   }
-
 // }
 
 // const point = new Point1(0, 0)
-// console.log(point.)
+// console.log(point)
 
-// ====== Экстендить
+// ====== Экстендить один класс от другого
 
 // class Point1 {
 //   x: number
@@ -69,15 +69,15 @@
 //   z: number
 
 //   constructor(x: number, y: number, z: number) {
-//     super(x, y)
+//     super(x, y) // с помощью super пробрасываем это конструктор в класс от которого он экстендиться.
 //     this.z = z
 //   }
 // }
 
 // const point = new D4Point(0, 0, 0)
-// // point. // x y z
-// ====== Экстендить немного про методы доступность
+// point. // x y z
 
+// ====== Экстендить немного про методы доступность
 // class Point1 {
 //   x: number
 //   y: number
@@ -87,7 +87,17 @@
 //     this.y = y
 //   }
 
-//  protected a() {} // Не будет доступен с наружи, но будет доступен в конструкторе класса D4Point
+//   //   private general() {
+//   //     console.log('general общий')
+//   //   }
+//   //   interior() {
+//   //     this.general()
+//   //   }
+
+//   protected closed() {
+//     console.log('closed')
+//   }
+//   // Не будет доступен с наружи, но будет доступен в конструкторе класса D4Point
 // }
 
 // class D4Point extends Point1 {
@@ -96,7 +106,6 @@
 //   constructor(x: number, y: number, z: number) {
 //     super(x, y)
 //     this.z = z
-
 //   }
 // }
 
@@ -104,65 +113,64 @@
 // point. // x y z
 
 // ====== Экстендить немного про методы переопределять
-class Point1 {
-  x: number
-  y: number
+// class Point1 {
+//   x: number
+//   y: number
 
-  constructor(x: number, y: number) {
-    this.x = x
-    this.y = y
-  }
+//   constructor(x: number, y: number) {
+//     this.x = x
+//     this.y = y
+//   }
 
-  a() {}
-}
+//   a() {}
+// }
 
-class D4Point extends Point1 {
-  z: number
+// class D4Point extends Point1 {
+//   z: number
 
-  constructor(x: number, y: number, z: number) {
-    super(x, y)
-    this.z = z
-  }
-  a(name?: string) {}
-}
+//   constructor(x: number, y: number, z: number) {
+//     super(x, y)
+//     this.z = z
+//   }
+//   a(name?: string) {} // Переопределили
+// }
 
-const point = new D4Point(0, 0, 0)
-// point. // x y z
+// const point = new D4Point(0, 0, 0)
+// point // x y z
 
 // ============== static
-class StaticTest {
-  static c = 'pet'
+// class StaticTest {
+//   static c = 'pet'
 
-  static test() {
-    console.log('test')
-  }
-}
+//   static test() {
+//     console.log('test')
+//   }
+// }
 
-const zx = StaticTest.c
-const zxTest = StaticTest.test()
+// const zx = StaticTest.c
+// const zxTest = StaticTest.test()
 
 // ===================
+// abstract class Test3 {
+//   method() {
+//     console.log('method')
+//   }
+// }
 
-abstract class Test3 {
-  method() {
-    console.log('method')
-  }
-}
+// // const abs = new Test3() // не можем так сделать так как это Test3() абстракт
 
-// const abs = new Test3() // не можем так сделать так как это Test3() абстракт
+// class Test4 extends Test3 {
+//   method4() {
+//     console.log('method')
+//   }
+// }
 
-class Test4 extends Test3 {
-  method4() {
-    console.log('method')
-  }
-}
-
-const test4 = new Test4()
-// test4. // method и method4
+// const test4 = new Test4()
+// test4 // method и method4
 
 // ==========  имплеминтация
 
-class Point2 {
+class Point1 {
   x: number
   y: number
 
