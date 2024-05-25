4764fd2f975989b2605b2c9a9fc734dd1772f029
Art San <artsan.mcc@gmail.com>
2024-05-25 18:55:27 +0700

    ok

---
+++ Ngrok/start.js
@@ -0,0 +1,19 @@
+// ngrok.yml
+// version: 1
+// authtoken: YOU NJRTN
+// tunnels:
+//   first_tunnel:
+//     proto: http
+//     addr: 5173
+//   second_tunnel:
+//     proto: http
+//     addr: 3001
+
+// Положил этот файл
+// C:\Users\Aleksandr\.ngrok2\ngrok.yml
+
+// Этой командой запускал
+// ngrok start --all --config=C:\Users\Aleksandr\.ngrok2\ngrok.yml
+
+// Эта не сработала
+// ngrok start --all
