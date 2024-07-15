6800f1804b4f631d04218f1b50a2bc9b3f47267d
Art San <artsan.mcc@gmail.com>
2024-07-15 14:50:41 +0700

    ok

---
+++ Создание нового проэкта REAKT.js
@@ -0,0 +1,22 @@
+// npx create-react-app "название проекта"
+//      если выдаст ошибку то
+// npx create-react-app@5.0.0 "название проекта"
+
+// Установка реакт ротар дом
+// npm i react-router-dom@5.3.0
+
+// REACT_APP_FIREBASE_KEY =
+// .env.development
+
+// https://frontend-15-firebase-default-rtdb.europe-west1.firebasedatabase.app/
+
+// Переменные окружения. DotEnv
+
+// VITE
+// npm create vite@latest
+
+// package.json
+// npm init -y
+
+// typescript
+// npm i typescript
