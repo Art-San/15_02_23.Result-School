47279cdcf1cc9aff8a2961bad0da266293755218
Art San <artsan.mcc@gmail.com>
2024-07-17 12:52:44 +0700

    25 штучек

---
+++ 25 тем которые надо знать.js
@@ -0,0 +1,27 @@
+// https://www.youtube.com/watch?v=__OZEJIeu4c&t=83s видео автора данного набора
+
+// 1. inheritance наследование
+// 2. prototype _proto_
+// 3. closures замыкание
+// 4. constructor factory
+// 5. this
+// 6. arrow functions стрелочные функции
+// 7. reference / ссылка  -- value / значение
+// 8. Асинхронное программирование
+// 9. Promises промисы
+// 10. hoisting
+// 11. destructuring деструктуризация
+// 12. template literals шаблонные строки
+// 13. типы данных
+// 14. event loop событийный цикл
+// 15. modules модули
+// 16. Асинхронные итераторы и генераторы
+// 17. методы массивов
+// 18. error handling
+// 19. веб браузер API  (Fetch, LocalStorage, sessionStorage)
+// 20. DOM
+// 21. event delegation
+// 22. работа с датами
+// 23. reges регулярные выражения
+// 24. DevTools
+// 25. Безопасность
