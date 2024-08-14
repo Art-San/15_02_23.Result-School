d39e89845507890b36b526bb2090240f0134e2f4
Art San <artsan.mcc@gmail.com>
2024-08-14 23:35:44 +0700

    ok

---
+++ Модуль 1. База JavaScript/Lesson_17.Callback + Event Loop/2.3 Event Loop-Example-YTb-Ulbi_TV.js
@@ -0,0 +1,16 @@
+// 2.3 Event Loop-Example-YTb-Ulbi_TV.js
+// function recursivePromise(promise) {
+//   return promise.then(() => {
+//     console.log('promise 1')
+//     recursivePromise(Promise.resolve())
+//   })
+// }
+
+// recursivePromise(Promise.resolve())
+
+// setTimeout(() => console.log('timeout'), 0)
+
+// for (let i = 0; i < Infinity; i++) {
+//   console.log(i)
+//   Promise.resolve().then(() => console.log(i))
+// }
