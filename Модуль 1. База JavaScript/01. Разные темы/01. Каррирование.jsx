4af18bd525841ceccdeba2bc689a6681a5902cd8
Art San <artsan.mcc@gmail.com>
2024-08-18 17:54:49 +0700

    ok

---
+++ Модуль 1. База JavaScript/01. Разные темы/01. Каррирование.jsx
@@ -0,0 +1,82 @@
+// https://www.youtube.com/watch?v=oDihI558xWc
+// Еще о каррировании https://it-dev-journal.ru/articles/chto-takoe-karrirovanie-funkczionalnoe-programmirovanie
+// А́рность (англ. arity) функции — количество ее аргументов.
+
+// 1.0 Каррирование — это уменьшение арности функций
+
+// 1.1 Каррирование функций — это преобразование функции с любым количеством аргументов
+// в функцию только с одним аргументом.
+
+// 1.2 Каррированная функция — это функция, которая продолжает возвращать функции до тех пор,
+// пока не будут отправлены все ее параметры.
+
+// Каррированая функция, это функция высшего порядка
+
+// Пример 1
+const add = (a, b) => a + b
+const add1 = (a) => (b) => a + b
+
+// Пример 2
+const mul = (a, b, c) => {
+  return a * b * c
+}
+
+console.log(mul(1, 2, 5))
+
+// Пример 3
+const curriedMu2 = (a) => (b) => (c) => {
+  return a * b * c
+}
+const curriedMul1 = function (a) {
+  return function (b) {
+    return function (c) {
+      return a * b * c
+    }
+  }
+}
+
+console.log(curriedMul2(1)(2)(5))
+
+// Пример 4 частичное применение
+
+const curriedMul = (a) => (b) => (c) => {
+  return a * b * c
+}
+const mulBy5 = curriedMul(5)
+
+console.log(mulBy5(2)(5))
+
+// Пример 5 Функция вызовов 2 а может быть и больше
+const log = (level) => (message) => console[level](message)
+
+// log
+// error
+const logInfo = log('log')
+const logError = log('error')
+
+logInfo('This is message info')
+logError('This is message error')
+
+// Задача
+// https://lodash.com/docs/4.17.15#curry
+const mul3 = (a, b, c) => {
+  return a * b * c
+}
+
+const curry = (fn) => {
+  const curried = (...args) => {
+    if (args.length >= fn.length) {
+      return fn(...args)
+    } else {
+      return (...args2) => curried(...args, ...args2)
+    }
+  }
+  return curried
+}
+
+const curriedMul3 = curry(mul3)
+
+console.log(curriedMul3(1)(2)(5))
+console.log(curriedMul3(1, 2)(5))
+console.log(curriedMul3(1, 2, 5))
+console.log(curriedMul3(1)(2, 5))
---
+++ Модуль 1. База JavaScript/01. Разные темы/Content-Security-Policy/test.js
@@ -0,0 +1,33 @@
+// src="https://hamsterkombatgame.io/clicker/#tgWebAppData=query_id%3DAAHMWgYrAAAAAMxaBis9Shya%26user%3D%257B%2522id%2522%253A721836748%252C%2522first_name%2522%253A%2522%25D0%2590%25D0%25BB%25D0%25B5%25D0%25BA%25D1%2581%25D0%25B0%25D0%25BD%25D0%25B4%25D1%2580%2522%252C%2522last_name%2522%253A%2522%25D0%2590%2522%252C%2522username%2522%253A%2522gruzz70tomsk%2522%252C%2522language_code%2522%253A%2522ru%2522%252C%2522allows_write_to_pm%2522%253Atrue%257D%26auth_date%3D1721380364%26hash%3D840e29abc0617def351ea805e3f012386a10a6fe5b0134d109a9f9be07a57565&tgWebAppVersion=7.6&tgWebAppPlatform=web&tgWebAppThemeParams=%7B%22bg_color%22%3A%22%23ffffff%22%2C%22button_color%22%3A%22%233390ec%22%2C%22button_text_color%22%3A%22%23ffffff%22%2C%22hint_color%22%3A%22%23707579%22%2C%22link_color%22%3A%22%2300488f%22%2C%22secondary_bg_color%22%3A%22%23f4f4f5%22%2C%22text_color%22%3A%22%23000000%22%2C%22header_bg_color%22%3A%22%23ffffff%22%2C%22accent_text_color%22%3A%22%233390ec%22%2C%22section_bg_color%22%3A%22%23ffffff%22%2C%22section_header_text_color%22%3A%22%233390ec%22%2C%22subtitle_text_color%22%3A%22%23707579%22%2C%22destructive_text_color%22%3A%22%23df3f40%22%7D"
+
+// query_id: Идентификатор запроса, который, вероятно,
+// используется сервером для отслеживания конкретных событий или действий пользователя.
+
+// user: Объект JSON, содержащий информацию о пользователе:
+
+// id: Уникальный идентификатор пользователя.
+
+// first_name: Имя пользователя.
+
+// last_name: Фамилия пользователя.
+
+// username: Имя пользователя в системе.
+
+// language_code: Код языка, используемый пользователем.
+
+// allows_write_to_pm: Параметр, указывающий,
+// разрешено ли пользователю отправлять личные сообщения.
+
+// auth_date: Дата и время аутентификации пользователя.
+
+// hash: Хэш, возможно,
+// используемый для проверки подлинности данных пользователя.
+
+// tgWebAppVersion, tgWebAppPlatform, tgWebAppThemeParams:
+// Параметры, связанные с версией, платформой и темой веб-приложения Telegram,
+// которое, судя по всему, интегрировано с сайтом.
+
+// Таким образом, эта ссылка не только направляет пользователя к игре,
+// но и передает значительную информацию о нем самому сайту,
+// что может быть использовано для персонализации пользовательского опыта,
+// аутентификации или других целей.
