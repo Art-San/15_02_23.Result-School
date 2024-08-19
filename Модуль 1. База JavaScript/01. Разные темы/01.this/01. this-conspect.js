80ef276f1acda5fca73d2c7afa21a207d90b8282
Art San <artsan.mcc@gmail.com>
2024-08-19 23:21:02 +0700

    ok

--- Модуль 1. База JavaScript/01. Разные темы/01.this/01. this-conspect.js
+++ Модуль 1. База JavaScript/01. Разные темы/01.this/01. this-conspect.js
@@ -1,38 +1,3 @@
-// src="https://hamsterkombatgame.io/clicker/#tgWebAppData=query_id%3DAAHMWgYrAAAAAMxaBis9Shya%26user%3D%257B%2522id%2522%253A721836748%252C%2522first_name%2522%253A%2522%25D0%2590%25D0%25BB%25D0%25B5%25D0%25BA%25D1%2581%25D0%25B0%25D0%25BD%25D0%25B4%25D1%2580%2522%252C%2522last_name%2522%253A%2522%25D0%2590%2522%252C%2522username%2522%253A%2522gruzz70tomsk%2522%252C%2522language_code%2522%253A%2522ru%2522%252C%2522allows_write_to_pm%2522%253Atrue%257D%26auth_date%3D1721380364%26hash%3D840e29abc0617def351ea805e3f012386a10a6fe5b0134d109a9f9be07a57565&tgWebAppVersion=7.6&tgWebAppPlatform=web&tgWebAppThemeParams=%7B%22bg_color%22%3A%22%23ffffff%22%2C%22button_color%22%3A%22%233390ec%22%2C%22button_text_color%22%3A%22%23ffffff%22%2C%22hint_color%22%3A%22%23707579%22%2C%22link_color%22%3A%22%2300488f%22%2C%22secondary_bg_color%22%3A%22%23f4f4f5%22%2C%22text_color%22%3A%22%23000000%22%2C%22header_bg_color%22%3A%22%23ffffff%22%2C%22accent_text_color%22%3A%22%233390ec%22%2C%22section_bg_color%22%3A%22%23ffffff%22%2C%22section_header_text_color%22%3A%22%233390ec%22%2C%22subtitle_text_color%22%3A%22%23707579%22%2C%22destructive_text_color%22%3A%22%23df3f40%22%7D"
-
-// query_id: Идентификатор запроса, который, вероятно,
-// используется сервером для отслеживания конкретных событий или действий пользователя.
-
-// user: Объект JSON, содержащий информацию о пользователе:
-
-// id: Уникальный идентификатор пользователя.
-
-// first_name: Имя пользователя.
-
-// last_name: Фамилия пользователя.
-
-// username: Имя пользователя в системе.
-
-// language_code: Код языка, используемый пользователем.
-
-// allows_write_to_pm: Параметр, указывающий,
-// разрешено ли пользователю отправлять личные сообщения.
-
-// auth_date: Дата и время аутентификации пользователя.
-
-// hash: Хэш, возможно,
-// используемый для проверки подлинности данных пользователя.
-
-// tgWebAppVersion, tgWebAppPlatform, tgWebAppThemeParams:
-// Параметры, связанные с версией, платформой и темой веб-приложения Telegram,
-// которое, судя по всему, интегрировано с сайтом.
-
-// Таким образом, эта ссылка не только направляет пользователя к игре,
-// но и передает значительную информацию о нем самому сайту,
-// что может быть использовано для персонализации пользовательского опыта,
-// аутентификации или других целей.
-// Александр
-
 // THIS - это индификатор (переменная (или, точнее, ссылка на объект))
 // значение которого устанавливается в зависимости от обстоятельств вызова функции или метода.
 
@@ -194,10 +159,6 @@
 
 // setTimeout(theObj.doLogThis, 1)
 
-// 1. v8 = undefined
-// 2. браузере = Window {...}
-// 3. node = Timeout {.....} // причудливый объект это хоз среда его туда запихала
-
 // 15. Пример наоборот - 3
 // const theObj = {
 //   name: 'Murych',
@@ -221,6 +182,20 @@
 // значение this по своему усмотрению, что иногда приводит
 // к неожиданным результатам и нарушению интуитивных ожиданий.
 
+// 17.
+const theObj = {
+  name: 'Murych',
+  doLogThis: function () {
+    console.log('this is: ', this)
+  }
+}
+
+setTimeout(theObj.doLogThis, 1)
+
+// 1. v8 = undefined
+// 2. браузере = Window {...}
+// 3. node = Timeout {.....} // причудливый объект это хоз среда его туда запихала
+
 // чтобы понять чему равен this нужно посмотреть где вызывается эта функция в нутри которой находится This
 // если мы находимся не внутри функции, то мы находимся либо в скрипте или модуле. Если в модуле это undefined,
 // если в скрипте то соглано спецификации host среды, которая может установить this какой ей хочется.
@@ -233,22 +208,3 @@
 // Если предыдушие пункты не подошли то  this = undefined
 
 // This это особый аргумент нормальной функции, нормальная функция вызывается минимум с одним аргументом и это this
-
-String.prototype.doThingStrict = function () {
-  'use strict'
-  console.log('this is:', this instanceof Object, this)
-}
-
-String.prototype.doThing = function () {
-  console.log('this is:', this instanceof Object, this)
-}
-
-Number.prototype.doThing = function () {
-  'use strict'
-  console.log('this is:', this)
-}
-
-// "Yo".doThingStrict(); 	// this будет связан со значением "Yo"
-// "Yo".doThing(); 		// this будет связан со значением new String("Yo")
-;(12).doThing()
-// doThing();
