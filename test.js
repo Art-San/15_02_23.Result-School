d1ca2dca7715584f1a803cbb00b3eaea5eafcc4b
Art San <artsan.mcc@gmail.com>
2024-08-26 21:54:18 +0700

    ok

--- test.js
+++ test.js
@@ -1,13 +1,13 @@
-// console.log(1, 0 && 'Hello')
-// console.log(2, 'Hi' && null && 42)
-// console.log(3, {} && [] && 'JavaScript')
-// console.log(4, 1 && true && {} && false && function () {})
-// console.log(5, 1 && true && {} && [] && function () {})
+// console.log(1, 0 && 'Hello') // 0
+// console.log(2, 'Hi' && null && 42) // null
+// console.log(3, {} && [] && 'JavaScript') // 'JavaScript'
+// console.log(4, 1 && true && {} && false && function () {}) // false
+// console.log(5, 1 && true && {} && [] && function () {}) // fun
 
 // // Сложнее
-// console.log(6, true && 'value' && 0 && 'not reached')
-// console.log(7, 1 && null && undefined && 'text')
-// console.log(8, 'start' && '' && 42 && false) //
+// console.log(6, true && 'value' && 0 && 'not reached') // 0
+// console.log(7, 1 && null && undefined && 'text') // null
+// console.log(8, 'start' && '' && 42 && false) // ''
 
 // console.log(9, 1 || 'да')
 // console.log(10, 0 || 'да')
@@ -19,151 +19,70 @@
 // console.log(15, null || 'nullish' || '' || undefined)
 // console.log(16, false || 0 || '' || 100 || 'end')
 
-// console.log(17, null ?? 'defined') //  'defined'
-// console.log(18, undefined ?? 0 ?? 'backup') // 0
-// console.log(19, 'not null' ?? 'fallback') // 'not null'
+// console.log(17, null ?? 'defined')
+// console.log(18, undefined ?? 0 ?? 'backup')
+// console.log(19, 'not null' ?? 'fallback')
 
 // // Сложнее
-// console.log(20, undefined ?? null ?? NaN ?? 0) // NaN
-// console.log(21, 0 ?? false ?? null ?? 'backup') // 0
-// console.log(22, '' ?? 'empty' ?? 'filled') // ''
+// console.log(20, undefined ?? null ?? NaN ?? 0)
+// console.log(21, 0 ?? false ?? null ?? 'backup')
+// console.log(22, '' ?? 'empty' ?? 'filled')
 
-// Жаргон: область видимости, мистическое лексическое окружение
+// Введение
 
-// В спецификации: цепочка окружения
-// Надо учить прототипную модель js
+// 5
+// 5 + 7
 
-// var theThing = 1
+// var leftBox = 5
+// var rightBox = 7
 
-// function doGlobalThing() {
-//   console.log(theThing)
-//   var theSomeThing = 10
-//   function newDoThing() {
-//     var theNewNewDoThing = 777
-//   }
-// }
-// function doThing(doGlobalThing) {
-//   var theThing = 2
-//   doGlobalThing()
-// }
+// leftBox + rightBox
+// var oneMoreBox = leftBox
+// leftBox = 11
 
-// doThing(doGlobalThing)
+// console.log(1, leftBox)
+// console.log(2, oneMoreBox)
 
-//                                                                                                  newDoThing
-//                                                                                                 ***************************************
-//                                                                                                 *                                     *
-//                                                                                          ------ * --[[ outer ]]                       *
-//                                                                                          |      *                                     *
-//                                                       doGlobalThing                      V      *   theNewNewDoThing                  *
-//                                                       * ************************************    *                                     *
-//                                                       *                                    *    *   (каробочка 3)                     *
-//                                   --------------------* -- [[ outer ]]                     *    *   *******************************   *
-//                                   |                   *                                    *    *   *                             *   *
-//                                   |                   *    theSomeThing                    *    *   * var theNewNewDoThing = 777  *   *
-//        Окружение - среда          V                   *                                    *    *   *                             *   *
-//      *********************************************    *    (каробочка 2)                   *    *   *                             *   *
-//      *                                           *    *    *****************************   *    *   *                             *   *
-//      *        theThing                           *    *    *                           *   *    *   *******************************   *
-//      *        doGlobalThing                      *    *    *  console.log(theThing)    *   *    *                                     *
-//      *        doThing                            *    *    *  var theSomeThing = 10    *   *    ***************************************
-//      *  (каробочка 1)                            *    *    *  function newDoThing() {  *   *
-//      *  ***************************************  *    *    *                           *   *
-//      *  *                                     *  *    *    *  }                        *   *
-//      *  *  var theThing = 1                   *  *    *    *                           *   *
-//      *  *  function doGlobalThing() {         *  *    *    *****************************   *
-//      *  *                                     *  *    *                                    *
-//      *  *  }                                  *  *    **************************************
-//      *  *  function doThing(doGlobalThing) {  *  *
-//      *  *                                     *  *
-//      *  *                                     *  *              doThing
-//      *  *  }                                  *  *             *********************************
-//      *  *                                     *  *             *                               *
-//      *  *  doThing(doGlobalThing)             *  * <-----------*-- [[ outer ]]                 *
-//      *  *                                     *  *             *                               *
-//      *  ***************************************  *             *    theThing                   *
-//      *                                           *             *                               *
-//      *********************************************             *    ************************   *
-//                                                                *    *                      *   *
-//                                                                *    *  var theThing = 2    *   *
-//                                                                *    *                      *   *
-//                                                                *    *  doGlobalThing()     *   *
-//                                                                *    ************************   *
-//                                                                *                               *
-//                                                                *********************************
+// ====================================
+// var megaBox = [5, 11, 9, 17, 1]
+// var oneMoreBox = megaBox
 
-// Когда среда JS анализирует этот код, он обнаруживает что есть
-// две function decloration (2 деклорации функции) doGlobalThing и doThing она создала
-// в окружение для них идентификаторы: theThing, doGlobalThing, doThing .
-// Но кроие этого оно создало для doGlobalThing коробочку с кодом console.log(theThing)
-// внутри своего собственного окружения.
-// Другими словами для Js для runtime согластно спецификации при выполнение этого
-// кода существуют только конструкции (смотри в коробочку 1 ),
-// кода в функций не существует. Этот код попадает в отдельную коробочку. При этом в
-// специальном служебном поле outer, прописывается связь между этим outer этм нашем окружением
-// и окружение где была рождена функция 27:30
+// oneMoreBox[0] = 7
 
-// doGlobalThing создает отдельно коробочку с кодом вокруг которой отдельно создается окружение и
-// у этого окружения именно в момент когда он прочитали function decloration в поле outer создается
-// связь с окружением где была рождена эта функция
+// console.log(1, megaBox)
+// console.log(2, oneMoreBox)
 
-///===================================================================================================
+// пример первый
+// var theThing = 0
+
+// var theList = [1, (theThing = 10), 2, theThing]
+
+// console.log(theList)
+
+// пример второй
+// var theThing = 0
+
+// var theList = [1, false && (theThing = 10), 2, theThing]
+
+// console.log(theList)
+
+// пример третий
+// var theThing = 0
+
+// var theList = [
+//   1,
+//   (false && (theThing = 10)) || (theThing = 20),
+//   theThing,
+//   false || (theThing = 10) || (theThing = 30),
+//   theThing
+// ]
+
+// [1, false, 0, 10, 10]
+
+// console.log(theList)
+
 var theThing = 1
-function doGlobalThing() {
-  var local = 7
-  return () => {
-    return local
-  }
-}
 
-var returnedFun = doGlobalThing()
+var doThing = () => ((theThing = 10), 1)
 
-function enotherFun(doThing) {
-  var local = 98
-  return doThing()
-}
-
-console.log(123, enotherFun(returnedFun))
-//                                                                                                  anon
-//                                                                                                 ***************************************
-//                                                                                                 *                                     *
-//                                                                                          ------ * --[[ outer ]]                       *
-//                                                                                          |      *                                     *
-//                                                       doGlobalThing                      V      *                                     *
-//                                                       * ************************************    *                                     *
-//                                                       *                                    *    *   (каробочка 3)                     *
-//                                   --------------------* -- [[ outer ]]                     *    *   *******************************   *
-//                                   |                   *                                    *    *   *                             *   *
-//                                   |                   *    local                           *    *   *                             *   *
-//        Окружение - среда          V                   *                                    *    *   * return local                *   *
-//      *********************************************    *    (каробочка 2)                   *    *   *                             *   *
-//      *                                           *    *    *****************************   *    *   *                             *   *
-//      *        theThing                           *    *    *                           *   *    *   *******************************   *
-//      *        doGlobalThing                      *    *    *  var local = 7            *   *    *                                     *
-//      *        enotherFun                         *    *    *  return (                 *   *    ***************************************
-//      *  (каробочка 1)                            *    *    *       () => {             *   *
-//      *  ***************************************  *    *    *      return local         *   *
-//      *  *                                     *  *    *    *      }                    *   *
-//      *  *  var theThing = 1                   *  *    *    *   )                       *   *
-//      *  *  function doGlobalThing() {         *  *    *    *****************************   *
-//      *  *     var local = 7                   *  *    *                                    *
-//      *  *    return (                         *  *    **************************************
-//      *  *         () => {                     *  *
-//      *  *             return local            *  *
-//      *  *     })                              *  *
-//      *  *   }                                 *  *              enotherFun
-//      *  *                                     *  *             ****************************
-//      *  *  var returnedFun = doGlobalThing()  *  * < --------- *----
-//      *  *                                     *  *             *   local
-//      *  *                                     *  *             *   doThing
-//      *  *  function enotherFun(doThing) {     *  *             *   **********************
-//      *  *  var local = 98                     *  *             *   *   var local = 98   *
-//      *  *  doThing()                          *  *             *   *                    *
-//      *  *   }                                 *  *             *   *   doThing()        *
-//      *  *                                     *  *             *   *                    *
-//      *  *    enotherFun(returnedFun)          *  *             *   *                    *
-//      *  ***************************************  *             *   *                    *
-//      *                                           *             *   ***********************
-//      *********************************************             *
-//                                                                ******************************
-//
-//
+console.log(doThing())
