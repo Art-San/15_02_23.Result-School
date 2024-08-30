351d71e5e920f4ce89771a72d56c23263bd49c98
Art San <artsan.mcc@gmail.com>
2024-08-30 15:49:13 +0700

    ok

--- Модуль 1. База JavaScript/01. Разные темы/13. Выполнение JS код/02. Как выполняется JS код.js
+++ Модуль 1. База JavaScript/01. Разные темы/13. Выполнение JS код/02. Как выполняется JS код.js
@@ -1,4 +1,17 @@
+function doThing() {
+  function doThingInner() {
+    debugger
+  }
+  debugger
+  doThingInner
+}
 console.log('hello  world')
 console.log('hello  world')
 console.log('hello  world')
 doThing()
+
+// Js помещает для каждого блока кода в Execution Context Stack ссылку на структуру Execution Context,
+// а эта структура содержит все необходимое, чтобы обеспечить работу базового единиц этого блока кода
+
+// Execution Context Stack это не тоже самое что CALL Stack у них разные правила попадание в них чего то ни было, и как следствие  порядок выполнения тоже разный
+// закончил 2:59
