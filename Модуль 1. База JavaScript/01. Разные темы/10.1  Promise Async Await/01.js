7ae9293001079142840c858dcf8da49b9a9aefc8
Art San <artsan.mcc@gmail.com>
2024-09-10 23:55:46 +0700

    ok

--- Модуль 1. База JavaScript/01. Разные темы/10.1  Promise Async Await/01.js
+++ Модуль 1. База JavaScript/01. Разные темы/10.1  Promise Async Await/01.js
@@ -1,2 +1,6 @@
 // Визуализация промисов и Async/Await
 // https://habr.com/ru/articles/501702/
+
+// Смотрим вместе YT: Григорий Бизюкин - Асинхронность ШРИ 2024
+// на 3:09  на обсуждение промиса
+// https://www.youtube.com/watch?v=vFSvq_ablAM&t=19s
