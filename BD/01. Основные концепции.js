ffe742efca8854be7f839fb4809cef757c92d987
Art San <artsan.mcc@gmail.com>
2024-09-17 18:28:09 +0700

    ok

---
+++ BD/01. Основные концепции.js
@@ -0,0 +1,12 @@
+// https://www.youtube.com/watch?v=HVQNxdI6fqY&list=PLBheEHDcG7-k1Y_Uy04Dj2ylWhcfSfqoF&index=1&t=4s
+
+// сновные клиент сервеные субд
+// MySQL
+// PostgreSQL
+// Oracle
+// MS SQL
+// MariaDB
+
+// все они реляционные
+// поддерживают язык SQL
+// (Structured Query Language - язык структурированных запросов)
