4703e4aaf6464890c27fff5ee5aeda8d3810055a
Art San <artsan.mcc@gmail.com>
2024-10-09 21:43:21 +0700

    ok

--- BD/Практический курс по SQL/035 INNER JOIN.js
+++ BD/Практический курс по SQL/035 INNER JOIN.js
@@ -17,8 +17,8 @@
 // INNER JOIN suppliers ON products.supplier_id = suppliers.supplier_id
 // ORDER BY units_in_stock DESC
 
-// Связываем колонку продукты и постовщиков через supplier_id в каждой таблице
-// В результироющем наборе получим три колонки название продукта, название компании поставщика,
+// Связываем колонку продукты и поставщиков через supplier_id в каждой таблице
+// В результирующем наборе получим три колонки название продукта, название компании поставщика,
 // кол единиц на складе отсортировано от большего к меньшему
 
 //     product_name,                     suppliers.company_name,              units_in_stock
@@ -34,6 +34,15 @@
 // ORDER BY SUM(units_in_stock) DESC
 // LIMIT 5
 
+// В результирующем наборе будет две колонки: первая колонка содержит названия категорий,
+// сгруппированных по всем продуктам, относящимся к каждой категории; вторая колонка содержит
+// сумму всех единиц товара на складе для каждой категории.
+
+// Для этого мы выполняем INNER JOIN таблицы products с таблицей categories по колонке category_id,
+// группируем результаты по имени категории, сортируем их по сумме единиц на складе
+// в порядке убывания и ограничиваем результат пятью строками.
+// ========================  ===================================================
+
 // SELECT category_name, SUM(unit_price * units_in_stock)
 // FROM products
 // INNER JOIN categories ON products.category_id = categories.category_id
@@ -42,14 +51,44 @@
 // HAVING SUM(unit_price * units_in_stock) > 5000
 // ORDER BY SUM(unit_price * units_in_stock) DESC
 
+// В результирующем наборе будет две колонки: первая колонка содержит названия категорий,
+// сгруппированных по всем продуктам, относящимся к каждой категории;
+//  вторая колонка содержит общую стоимость всех товаров, относящихся к данной категории.
+//  Выбираются только те категории, у которых общая стоимость товаров превышает 5000,
+//  и результаты отсортированы в порядке убывания.
+
+// ========================  ===================================================
 // SELECT order_id, customer_id, first_name, last_name, title
 // FROM orders
 // INNER JOIN employees ON orders.employee_id = employees.employee_id
+
+// В данном запросе объединяются данные из двух таблиц: orders и employees. В результате мы получаем следующие колонки:
+
+// order_id — айди заказа.
+// customer_id — айди клиента.
+// first_name — имя сотрудника, связанного с этим заказом.
+// last_name — фамилия сотрудника, связанного с этим заказом.
+// title — должность (тайтл) сотрудника, связанного с этим заказом.
+// INNER JOIN объединяет таблицы по колонке employee_id, чтобы связать заказы с соответствующими сотрудниками.
+
+// ========================  ===================================================
 
 // SELECT order_date, product_name, ship_country, products.unit_price, quantity, discount
 // FROM orders
 // INNER JOIN order_details ON orders.order_id = order_details.order_id
 // INNER JOIN products ON order_details.product_id = products.product_id
+
+// В данном запросе объединяются данные из трёх таблиц: orders, order_details и products. В результате мы получаем следующие колонки:
+
+// order_date — дата заказа.
+// product_name — название продукта.
+// ship_country — страна доставки.
+// products.unit_price — цена за единицу продукта.
+// quantity — количество заказанного продукта.
+// discount — скидка на продукт.
+// INNER JOIN объединяет таблицы по соответствующим колонкам, чтобы связать заказы с их деталями и продуктами.
+
+// ========================  ===================================================
 
 // SELECT contact_name, company_name, phone, first_name, last_name, title,
 //        order_date, product_name, ship_country, products.unit_price, quantity, discount
